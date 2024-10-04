6dc3d0f30d10daa7081d2d884b0d3cb49ce4c208
Niteesha Ayela <niteesha@prestigeconstructions.com>
2024-10-04 06:44:24 +0530

    Initial commit

---
+++ src/Oct/ex_02102024/Lab001.java
@@ -0,0 +1,8 @@
+package Oct.ex_02102024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+//         This is my first program practive
+        System.out.println("Hello");
+    }
+}
